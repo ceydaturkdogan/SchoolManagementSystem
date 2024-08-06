ebe38a50898d9233e49dcf4d41ae1deae976f126
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-06 15:56:31 +0300

    add some update

--- SchoolManagementSystem/Program.cs
+++ SchoolManagementSystem/Program.cs
@@ -9,12 +9,39 @@ namespace SchoolManagementSystem
         static void Main(string[] args)
         {
 
-            Person student = new Person();
-            student.Name = "Ceyda";
-            student.SurName = "Türkdoğan";
-            student.BirthDate = new DateTime(1995,01,21);
+            Person firstStudent = new Person() {
+            Name = "Ali",
+            SurName = "Acar",
+            BirthDate = new DateTime(1995,01,21)
+            };
 
-            student.ShowInformation();
+            Person secondStudent = new Person() { 
+            Name = "Veli",
+            SurName = "Çetinkaya",
+            BirthDate = new DateTime(1995, 04, 21)
+            };
+           
+            Person firstTeacher = new Person() { 
+            Name = "Semra",
+            SurName = "Yılmaz",
+            BirthDate = new DateTime(1995, 03, 21)
+            };
+            
+            Person secondTeacher = new Person()
+            {
+                Name = "Esra",
+                SurName = "Sayın",
+                BirthDate = new DateTime(1995,02 ,21)
+            };
+
+
+
+
+
+            firstStudent.ShowInformation();
+            secondStudent.ShowInformation();
+            firstTeacher.ShowInformation();
+            secondTeacher.ShowInformation();
 
         }
     }
