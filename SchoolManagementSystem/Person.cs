fa9b89b3ec81eddbab801ca8895d0d7285ffb6b5
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-06 15:05:36 +0300

    Add project files.

---
+++ SchoolManagementSystem/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem
+{
+    public class Person
+    {
+        private string name;
+        private string surName;
+        private DateTime birthDay;
+        public string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name=value;
+            }
+        }
+
+        public string SurName
+        {
+            get
+            { return surName;
+            }
+
+            set
+            {
+                surName=value;
+            }
+        }
+
+        public DateTime BirthDate {
+            get 
+            {
+                return birthDay;
+            }
+
+            set
+            {
+
+                birthDay=value;
+            }
+        
+        } 
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"İsim:{Name}\nSoyisim:{SurName}\nDoğum Tarihi:{BirthDate}");
+        }
+    }
+}
---
+++ SchoolManagementSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagementSystem;
+
+static void Main (string[] args)
+{
+
+    Person person = new Person();
+    person.Name = "Ceyda";
+    person.SurName = "Türkdoğan";
+    person.BirthDate = new DateTime(1995, 1, 21);
+
+    person.DisplayInfo();
+
+}
